d9f539380e2bfd5b1a449efbb6f1cbdd00e16686
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-16 01:36:04 +0300

    feat: fetch status.json in app.vue & show sonner

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { onMounted } from 'vue';
 import BrowserDetector from 'browser-dtector';
 import { useGuideStore } from '@/stores/guide';
+import { toast, Toaster } from 'vue-sonner';
 
 const guideStore = useGuideStore();
 
@@ -11,9 +12,31 @@ onMounted(() => {
 
   guideStore.setBrowser(browserName);
   guideStore.setUrlOrigin(window.location.origin);
+
+  const promise = () => {
+    return new Promise((resolve, reject) => {
+      fetch('https://update.itstpm.tech/status')
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          resolve(data);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+          reject(error);
+        });
+    });
+  };
+
+  toast.promise(promise(), {
+    loading: 'Загрузка данных с сервера...',
+    success: 'Расширение готово к установке!',
+    error: 'Ошибка при загрузке данных!',
+  });
 });
 </script>
 
 <template>
+  <Toaster position="top-right" theme="dark" />
   <RouterView />
 </template>
