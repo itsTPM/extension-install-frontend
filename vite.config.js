e5e7a43a51f1bdeca71191d030df428cd3f64d76
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-15 18:36:46 +0300

    feat: init shadcn-vue cli

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+});
