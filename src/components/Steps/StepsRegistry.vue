ab39c704f0f43f1c5ac33ca93e67e899e8b0a57e
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-16 00:55:26 +0300

    feat: reworked StepsRegistry page

--- src/components/Steps/StepsRegistry.vue
+++ src/components/Steps/StepsRegistry.vue
@@ -19,11 +19,15 @@ const actualPath = data?.find((item) => item.name === guideStore.browser)?.path 
 </script>
 
 <template>
-  <span>
-    Выполните следующие команды в командной строке. Они добавят настройку в Chrome, разрешающую установку расширения:
-  </span>
-  <CodeBlock>reg add {{ actualPath }}\ExtensionInstallAllowlist /v 1 /t REG_SZ /d {{ guideStore.extId }} /f</CodeBlock>
-  <CodeBlock>
-    reg add {{ actualPath }}\ExtensionInstallSources /v 1 /t REG_SZ /d {{ guideStore.urlOrigin }}/* /f
-  </CodeBlock>
+  <div class="flex flex-col gap-3">
+    <span>
+      Выполните следующие команды в командной строке. Они добавят настройку в Chrome, разрешающую установку расширения:
+    </span>
+    <CodeBlock>
+      reg add {{ actualPath }}\ExtensionInstallAllowlist /v 1 /t REG_SZ /d {{ guideStore.extId }} /f
+    </CodeBlock>
+    <CodeBlock>
+      reg add {{ actualPath }}\ExtensionInstallSources /v 1 /t REG_SZ /d {{ guideStore.urlOrigin }}/* /f
+    </CodeBlock>
+  </div>
 </template>
