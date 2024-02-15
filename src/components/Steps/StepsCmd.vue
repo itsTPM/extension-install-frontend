aabcba6e70be81866ffe0c5a61f568b7c01f70ad
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-16 00:49:05 +0300

    feat: reworked StepsCmd page

--- src/components/Steps/StepsCmd.vue
+++ src/components/Steps/StepsCmd.vue
@@ -3,12 +3,16 @@ import { Badge } from '@/components/ui/badge/index.js';
 </script>
 
 <template>
-  <span>
-    Одновременно нажмите
-    <Badge>Win + R</Badge>
-    , введите
-    <Badge>cmd</Badge>
-    и снова одновременно нажмите
-    <Badge>Ctrl + Shift + Enter</Badge>
-  </span>
+  <ul class="flex list-inside list-['➞'] flex-col gap-4">
+    <li>
+      Одновременно нажмите
+      <Badge>Win + R</Badge>
+      и введите
+      <Badge>cmd</Badge>
+    </li>
+    <li>
+      Затем снова одновременно нажмите
+      <Badge class="font-bold underline underline-offset-4">Ctrl + Shift + Enter</Badge>
+    </li>
+  </ul>
 </template>
