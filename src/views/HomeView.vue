9c0bc4ea1ce963fe8e1bc99d950b9860243d9c83
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-18 01:15:06 +0300

    feat: change buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card/index.js';
-import BrowserButton from '@/components/BrowserButton.vue';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card/index.js';
 import { useGuideStore } from '@/stores/guide.js';
 import { browsersData } from '@/browsers.js';
-import GitHubButton from '@/components/GitHubButton.vue';
+import { Button } from '@/components/ui/button/index.js';
+import { IconBrandGithub, IconDownload } from '@tabler/icons-vue';
 
 const guideStore = useGuideStore();
 
@@ -11,23 +11,27 @@ const currentBrowser = browsersData.find((browser) => browser.name === guideStor
 </script>
 
 <template>
-  <Card class="relative flex h-fit flex-col gap-4 p-10">
-    <CardHeader class="flex-shrink flex-col items-center gap-4 p-0">
-      <img alt="" class="max-w-[200px]" src="../../public/logo.svg" />
+  <Card class="relative flex h-fit flex-col gap-6 p-10">
+    <CardHeader class="flex-shrink flex-col items-center gap-6 p-0">
+      <img alt="" class="max-w-48" src="../../public/logo.svg" />
       <div class="text-center">
-        <CardTitle class="text-4xl font-normal">Foxford Tools</CardTitle>
+        <CardTitle class="text-3xl font-normal">Foxford Tools</CardTitle>
         <CardDescription>кастомизация сайта Фокса</CardDescription>
       </div>
     </CardHeader>
-    <CardContent class="flex flex-grow flex-col gap-2 overflow-auto p-0">
-      <BrowserButton :browser="currentBrowser.displayName" url="/install" />
-      <GitHubButton url="https://github.com/itsTPM/foxford-tools" />
+    <CardContent class="flex flex-grow flex-col gap-3 overflow-auto p-0">
+      <RouterLink to="/install">
+        <Button class="flex h-fit w-full justify-between py-[0.825rem]">
+          <span class="text-xl font-normal">установить</span>
+          <IconDownload size="32" strokeWidth="1.5" />
+        </Button>
+      </RouterLink>
+      <a href="https://github.com/itsTPM/foxford-tools">
+        <Button class="flex h-fit w-full justify-between py-[0.825rem]">
+          <span class="text-xl font-normal">GitHub</span>
+          <IconBrandGithub size="32" strokeWidth="1.5" />
+        </Button>
+      </a>
     </CardContent>
-    <CardFooter class="flex-shrink p-0">
-      <span class="w-full text-center text-sm text-muted-foreground">
-        другой браузер? нажмите
-        <span class="cursor-pointer text-primary transition hover:text-muted-foreground">сюда</span>
-      </span>
-    </CardFooter>
   </Card>
 </template>
