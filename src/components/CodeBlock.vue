7ba3993ec8b7404f2eefd4361897cff95201b23f
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-22 00:48:11 +0300

    feat: set break-all for CodeBlock

--- src/components/CodeBlock.vue
+++ src/components/CodeBlock.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full overflow-auto rounded-lg border bg-primary-foreground p-3">
+  <div class="w-full overflow-auto break-all rounded-lg border bg-primary-foreground p-3">
     <slot></slot>
   </div>
 </template>
