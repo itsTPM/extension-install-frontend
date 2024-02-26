4eb72a13328bb900764bb65ea3daf31fbc224f90
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-26 23:53:15 +0300

    feat(changelog): add fetch from github api

--- src/views/ChangelogView.vue
+++ src/views/ChangelogView.vue
@@ -1,3 +1,23 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue';
 
-<template>Changelog</template>
+let data = ref([]);
+
+onMounted(async () => {
+  data.value = await fetch('https://api.github.com/repos/itsTPM/foxford-tools/releases').then((response) =>
+    response.json()
+  );
+});
+</script>
+
+<template>
+  <div>
+    <h1>Changelog</h1>
+    <ul>
+      <li v-for="release in data" :key="release.id">
+        <h2>{{ release.name }}</h2>
+        <p>{{ release.body }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
