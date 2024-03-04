868278814834fe88e3ef7658169466bb9d380b10
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-03-04 17:57:56 +0300

    chore: add 'guide' param to link for logging on backend

--- src/components/Steps/StepsInstall.vue
+++ src/components/Steps/StepsInstall.vue
@@ -15,7 +15,7 @@ const props = defineProps({
 <template>
   <div v-if="installMethod === 'oneClick'" class="flex flex-col gap-3">
     <span>Нажмите на кнопку и подтвердите установку во всплывающем окне</span>
-    <a :href="`${guideStore.apiUrl}/extension.crx`" class="w-fit">
+    <a :href="`${guideStore.apiUrl}/extension.crx?guide=true`" class="w-fit">
       <Button class="w-fit">Установить расширение</Button>
     </a>
   </div>
@@ -23,7 +23,7 @@ const props = defineProps({
   <div v-else-if="installMethod === 'dragAndDrop'" class="flex flex-col gap-3">
     <span>
       Правой кнопкой мыши нажмите на
-      <a :href="`${guideStore.apiUrl}/extension.crx`" class="w-fit">
+      <a :href="`${guideStore.apiUrl}/extension.crx?guide=true`" class="w-fit">
         <Button class="w-fi h-fit p-1 text-rose-300 transition hover:text-rose-400" variant="link"
           >вот эту ссылку</Button
         > </a
