a66d0a5e0741b629bde7f337561d4fa8b7ba4504
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-03-10 13:05:16 +0300

    refactor: use tailwind variables, add alt to logo, remove useless classes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card/index.js';
 import { Button } from '@/components/ui/button/index.js';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card/index.js';
 import { IconBrandGithub, IconDownload } from '@tabler/icons-vue';
 </script>
 
 <template>
   <Card class="flex flex-col gap-6 p-10">
     <CardHeader class="w-full gap-6 p-0">
-      <img alt="" class="h-[6.5rem] w-48" src="/logo.svg" />
+      <img alt="Foxford Tools Logo" class="w-48" src="/logo.svg" />
       <div class="text-center">
         <CardTitle class="text-3xl font-normal">Foxford Tools</CardTitle>
         <CardDescription>кастомизация сайта Фокса</CardDescription>
@@ -16,14 +16,14 @@ import { IconBrandGithub, IconDownload } from '@tabler/icons-vue';
 
     <CardContent class="flex flex-col gap-3 p-0">
       <RouterLink to="/install">
-        <Button class="h-fit w-full justify-between py-[0.825rem]">
+        <Button class="h-fit w-full justify-between py-3">
           <span class="text-xl font-normal">установить</span>
           <IconDownload size="2rem" strokeWidth="1.5" />
         </Button>
       </RouterLink>
 
       <a href="https://github.com/itsTPM/foxford-tools" target="_blank">
-        <Button class="h-fit w-full justify-between py-[0.825rem]">
+        <Button class="h-fit w-full justify-between py-3">
           <span class="text-xl font-normal">GitHub</span>
           <IconBrandGithub size="2rem" strokeWidth="1.5" />
         </Button>
