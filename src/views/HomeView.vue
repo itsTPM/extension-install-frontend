e6f4cfa9823ccbcbbffd47824234c757196496fd
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-05-22 02:06:10 +0300

    fix(homeView): semantic errors & simplify document structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,34 +2,29 @@
 import { Button } from '@/components/ui/button/index.js';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card/index.js';
 import { IconBrandGithub, IconDownload } from '@tabler/icons-vue';
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
   <Card class="flex flex-col gap-6 p-10">
-    <CardHeader class="w-full gap-6 p-0">
-      <div class="h-28 w-48">
+    <CardHeader class="w-full gap-0 p-0 text-center">
+      <div class="mb-6 h-28 w-48">
         <img alt="Foxford Tools Logo" class="h-full w-48" src="/logo.svg" />
       </div>
-      <div class="text-center">
-        <CardTitle class="text-3xl font-normal">Foxford Tools</CardTitle>
-        <CardDescription>кастомизация сайта Фокса</CardDescription>
-      </div>
+      <CardTitle class="text-3xl font-normal">Foxford Tools</CardTitle>
+      <CardDescription>кастомизация сайта Фокса</CardDescription>
     </CardHeader>
 
     <CardContent class="flex flex-col gap-3 p-0">
-      <RouterLink to="/install">
-        <Button class="h-fit w-full justify-between py-3">
-          <span class="text-xl font-normal">установить</span>
-          <IconDownload size="2rem" stroke-width="1.5" />
-        </Button>
-      </RouterLink>
+      <Button class="h-fit w-full justify-between py-3" to="/install" :as="RouterLink">
+        <p class="text-xl font-normal">установить</p>
+        <IconDownload size="2rem" stroke-width="1.5" aria-hidden="true" />
+      </Button>
 
-      <a href="https://github.com/itsTPM/foxford-tools" target="_blank">
-        <Button class="h-fit w-full justify-between py-3">
-          <span class="text-xl font-normal">GitHub</span>
-          <IconBrandGithub size="2rem" stroke-width="1.5" />
-        </Button>
-      </a>
+      <Button class="h-fit w-full justify-between py-3" as="a" href="https://github.com/itsTPM/foxford-tools">
+        <p class="text-xl font-normal">GitHub</p>
+        <IconBrandGithub size="2rem" stroke-width="1.5" aria-hidden="true" />
+      </Button>
     </CardContent>
   </Card>
 </template>
